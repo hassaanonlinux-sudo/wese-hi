cbfbd22250071739c214a6d038252bd38846f50f
hassaanonlinux-sudo <hassaanonlinux@gmail.com>
2026-08-12 17:55:34 +0500

    Testing live ngrok webhook

--- wese hi/src/App.jsx
+++ wese hi/src/App.jsx
@@ -71,7 +71,7 @@ export default function MultiStepForm() {
   };
 
   const stepsConfig = [
-    { label: 'Personal', num: 1 },
+    { label: "Personal", num: 1 },
     { label: 'Address', num: 2 },
     { label: 'Settings', num: 3 },
     { label: 'Review', num: 4 },
